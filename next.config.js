e2b2d7e5da44a6bcd835875d0db137cf68cb3347
Diego Feijó <diego.feijo13@gmail.com>
2024-05-30 11:56:01 -0300

    Moving repo

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+})
+
+module.exports = withNextra({
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  output: "export",
+  distDir: 'dist'
+})
